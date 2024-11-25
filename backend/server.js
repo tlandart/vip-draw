65df99ac0cfe94446be4794269983e41df10501a
tlandart <76754714+tlandart@users.noreply.github.com>
2024-11-25 16:20:45 -0500

    Testing CORS

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,9 @@ app.use(
 // });
 
 app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  req.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
