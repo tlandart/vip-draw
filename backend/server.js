39f8964561aa12439db91051637c7981e8ef379d
tlandart <76754714+tlandart@users.noreply.github.com>
2024-11-26 03:51:54 -0500

    Fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const client = new OAuth2Client(CLIENT_ID);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND);
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
