c79d0233708f7438870f0da80774aa101a9e6bd1
tlandart <76754714+tlandart@users.noreply.github.com>
2024-11-26 02:48:59 -0500

    Edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # run this while SSHed into the server VM
+docker compose down --remove-orphans
+
 docker build --squash -t frontend -f frontend.dockerfile .
 
 docker build --squash -t backend -f backend.dockerfile .
-
-docker compose down --remove-orphans
 
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 
