e77fca18d88554c539623a29f37198829a6ffcc2
tlandart <76754714+tlandart@users.noreply.github.com>
2024-11-26 02:34:53 -0500

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# run this while SSHed into the server VM
+docker build --squash -t frontend -f frontend.dockerfile .
+
+docker build --squash -t backend -f backend.dockerfile .
+
+docker-compose down --remove-orphans
+
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+docker-compose up -d
