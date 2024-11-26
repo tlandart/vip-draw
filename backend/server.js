3a28339f88bc3fd6c7e7db329e3d53fb8fd4751f
tlandart <76754714+tlandart@users.noreply.github.com>
2024-11-26 01:07:17 -0500

    Fix createClient parameters

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,14 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "*");
   next();
 });
+
 console.log("process.env.REDIS_HOST value is ", process.env.REDIS_HOST);
+
 const redisClient = createClient({
-  host: process.env.REDIS_HOST,
-  port: 6379,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: 6379,
+  },
 });
 
 redisClient.on("connect", () => {
